72f8cd42faab9efd9677953911a0d47a93113eaa
Dale Nguyen <dungnq@itbox4vn.com>
2018-10-15 21:50:34 -0400

    Initiated Selenium JavaScript project

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Automation Testing with Selenium and JavaScript
+
+This is a guide to UI Automation Testing by using Selenium and JavaScript
+
+## Getting Started
+
+You need to install either [Yarn](https://yarnpkg.com/en/) or [Node](https://nodejs.org/en/) on your machine. In this project, I will use Yarn for installing packages and running scripts.
+
+```sh
+yarn install
+```
+
+## Running UI Test
+
+```sh
+yarn run test
+```
---
+++ test.js
@@ -0,0 +1,22 @@
+require('chromedriver');
+const assert = require('assert');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+
+describe('Checkout Google.com', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    it('Search on Google', async function() {
+        await driver.get('https://google.com');
+        await driver.findElement(By.id('lst-ib')).click();
+        await driver.findElement(By.id('lst-ib')).sendKeys('dalenguyen', Key.RETURN);
+        // await driver.wait(until.titleIs('dalenguyen - Google Search'), 1000);
+        await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
+
+        let title = await driver.getTitle();
+        assert.equal(title, 'dalenguyen - Google Search');      
+    })
+})
